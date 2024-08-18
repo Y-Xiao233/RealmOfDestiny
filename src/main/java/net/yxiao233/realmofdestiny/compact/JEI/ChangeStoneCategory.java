a162d983186fb5636448172c01c113be5b124a37
Y-Xiao233 <1870377449@qq.com>
2024-08-18 21:06:33 +0800

    init

--- src/main/java/net/yxiao233/realmofdestiny/compact/JEI/ChangeStoneCategory.java
+++ src/main/java/net/yxiao233/realmofdestiny/compact/JEI/ChangeStoneCategory.java
@@ -3,21 +3,17 @@ package net.yxiao233.realmofdestiny.compact.JEI;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.yxiao233.realmofdestiny.ModRegistry.ModBlocks;
 import net.yxiao233.realmofdestiny.ModRegistry.ModItems;
 import net.yxiao233.realmofdestiny.RealmOfDestiny;
 import net.yxiao233.realmofdestiny.recipes.ChangeStoneRecipe;
-import net.yxiao233.realmofdestiny.recipes.GemPolishingRecipe;
 import org.jetbrains.annotations.Nullable;
 
 public class ChangeStoneCategory implements IRecipeCategory<ChangeStoneRecipe> {
@@ -64,10 +60,5 @@ public class ChangeStoneCategory implements IRecipeCategory<ChangeStoneRecipe> {
             ItemStack stack = changeStoneRecipe.getIngredients().get(i).getItems()[0];
             iRecipeLayoutBuilder.addSlot(RecipeIngredientRole.OUTPUT,x,y).addItemStack(stack);
         }
-    }
-
-    @Override
-    public void draw(ChangeStoneRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
     }
 }
