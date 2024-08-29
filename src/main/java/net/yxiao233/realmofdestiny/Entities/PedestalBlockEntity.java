5a37a2e7b5b7cf1bfdba0e05b5cfb865e09d53cd
Y-Xiao233 <1870377449@qq.com>
2024-08-29 16:38:10 +0800

    init

--- src/main/java/net/yxiao233/realmofdestiny/Entities/PedestalBlockEntity.java
+++ src/main/java/net/yxiao233/realmofdestiny/Entities/PedestalBlockEntity.java
@@ -2,29 +2,22 @@ package net.yxiao233.realmofdestiny.Entities;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.Container;
 import net.minecraft.world.Containers;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.common.util.NonNullConsumer;
 import net.minecraftforge.energy.EnergyStorage;
 import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.items.IItemHandler;
@@ -37,10 +30,7 @@ import net.yxiao233.realmofdestiny.recipes.PedestalGeneratorRecipe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.plaf.basic.BasicComboBoxUI;
-import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Random;
 
 public class PedestalBlockEntity extends BlockEntity {
     public ItemStackHandler itemHandler = new ItemStackHandler(1){
