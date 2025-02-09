b06ac4ca9625c9a6c6791e4417ecc7b4590fc133
Y-Xiao233 <1870377449@qq.com>
2025-02-09 20:09:33 +0800

    v2.0.401

--- src/main/java/net/yxiao233/realmofdestiny/api/block/entity/AbstractProcessingBlockEntity.java
+++ src/main/java/net/yxiao233/realmofdestiny/api/block/entity/AbstractProcessingBlockEntity.java
@@ -1,7 +1,6 @@
 package net.yxiao233.realmofdestiny.api.block.entity;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
